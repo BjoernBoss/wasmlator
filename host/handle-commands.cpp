33f8c45c72cffa0426ec339e580009cf9e795fb4
Bjoern Boss Henrichsen <bjoernbossdev@gmail.com>
2025-03-04 03:45:13 +0100

    Update to use the newest changes of arger

--- host/handle-commands.cpp
+++ host/handle-commands.cpp
@@ -5,13 +5,23 @@
 #include "../system/system.h"
 #include "../rv64/rv64-cpu.h"
 
+enum class GroupId : uint8_t {
+	start, destroy, step, run, until, echo, common, delBreak, delPrint, addBreak,
+	inBreak, inPrint, inReg, inInst, inMem8, inMem16, inMem32, inMem64, inEval
+};
+enum class OptionId : uint8_t {
+	debug, environment, depth, trace, log, bind, description
+};
 static arger::Config Commands{
 	arger::GroupName{ L"command" },
 	arger::Help{
 		L"Expression",
 		L"Addresses can be given as expressions of subtraction / addition of constants or registers.",
 	},
-	arger::Group{ L"start", L"",
+	arger::HelpEntry{ L"help",
+		arger::Description{ L"Print this help menu." },
+	},
+	arger::Group{ L"start", GroupId::start,
 		arger::Description{ L"Start a translation process." },
 		arger::Positional{ L"binary",
 			arger::Primitive::any,
@@ -22,17 +32,17 @@ static arger::Config Commands{
 			L"Arguments to be passed to the process."
 		},
 		arger::Require::AtLeast(1),
-		arger::Option{ L"debug",
+		arger::Option{ L"debug", OptionId::debug,
 			arger::Abbreviation{ L'd' },
 			arger::Description{ L"Configure the system to startup in debug mode." },
 		},
-		arger::Option{ L"environment",
+		arger::Option{ L"environment", OptionId::environment,
 			arger::Abbreviation{ L'e' },
 			arger::Description{ L"Add entry to environment vector (must be of form name=value)." },
 			arger::Payload{ L"key-value", arger::Primitive::any },
 			arger::Constraint{ [](const arger::Parsed& o) -> std::wstring {
-				for (size_t i = 0; i < o.options(L"environment"); ++i) {
-					std::wstring value = o.option(L"environment", i).value().str();
+				for (size_t i = 0; i < o.options(OptionId::environment); ++i) {
+					std::wstring value = o.option(OptionId::environment, i).value().str();
 					size_t index = value.size();
 					for (size_t i = 0; i < value.size(); ++i) {
 						if (value[i] != L'=')
@@ -50,131 +60,127 @@ static arger::Config Commands{
 			}},
 			arger::Require::Any(),
 		},
-		arger::Option{ L"log",
+		arger::Option{ L"log", OptionId::log,
 			arger::Abbreviation{ L'l' },
 			arger::Description{ L"Log all WAT blocks being generated." },
 		},
-		arger::Option{ L"trace",
+		arger::Option{ L"trace", OptionId::trace,
 			arger::Abbreviation{ L't' },
 			arger::Require{},
 			arger::Payload{L"mode", arger::Enum{
-				{ L"inst", L"Trace each executed instruction." },
-				{ L"chunk", L"Trace each entered instruction chunk." },
-				{ L"block", L"Trace each entered super-block." },
-				{ L"none", L"Do not perform any tracing." },
+				{ L"inst", arger::EnumValue{ L"Trace each executed instruction.", gen::TraceType::instruction } },
+				{ L"chunk", arger::EnumValue{ L"Trace each entered instruction chunk.", gen::TraceType::chunk } },
+				{ L"block", arger::EnumValue{ L"Trace each entered super-block.", gen::TraceType::block } },
+				{ L"none", arger::EnumValue{ L"Do not perform any tracing.", gen::TraceType::none } },
 			},
 			arger::Value{ L"none" } },
 			arger::Description{ L"Log the address of all blocks being entered." },
 		},
-		arger::Option{ L"depth",
+		arger::Option{ L"depth", OptionId::depth,
 			arger::Payload{L"count", arger::Primitive::unum, arger::Value{ uint64_t(sys::DefTranslationDepth) } },
 			arger::Description{ L"Configure the depth to which super-blocks should be crawled and translated at once." },
 		},
 	},
-	arger::Group{ L"destroy", L"",
+	arger::Group{ L"destroy", GroupId::destroy,
 		arger::Description{ L"Destroy the currently configured translation process." },
 	},
-	arger::Group{ L"help", L"",
-		arger::HelpFlag{},
-		arger::Description{ L"Print this help menu." },
-	},
-	arger::Group{ L"step", L"",
+	arger::Group{ L"step", GroupId::step,
 		arger::Abbreviation{ L's' },
 		arger::Description{ L"Step for the number of instructions." },
 		arger::Positional{ L"count", arger::Primitive::unum, L"Number of instructions to execute.", arger::Value{ 1 } },
 	},
-	arger::Group{ L"run", L"",
+	arger::Group{ L"run", GroupId::run,
 		arger::Abbreviation{ L'r' },
 		arger::Description{ L"Continue running until a breakpoint or other exception occurs." },
 	},
-	arger::Group{ L"until", L"",
+	arger::Group{ L"until", GroupId::until,
 		arger::Abbreviation{ L'u' },
 		arger::Description{ L"Continue running until a breakpoint or the given address has been reached." },
 		arger::Positional{ L"address", arger::Primitive::any, L"Address to halt at." },
 	},
-	arger::Group{ L"inspect", L"",
+	arger::Group{ L"inspect",
 		arger::Abbreviation{ L'i' },
 		arger::Description{ L"Inspect the state of the current cpu." },
-		arger::Option{ L"bind",
+		arger::Option{ L"bind", OptionId::bind,
 			arger::Abbreviation{ L'b' },
 			arger::Description{ L"Bind the inspection to be printed on every debug halt." },
 		},
-		arger::Group{ L"break", L"in-break",
+		arger::Group{ L"break", GroupId::inBreak,
 			arger::Description{ L"Print the set breakpoints." },
 		},
-		arger::Group{ L"print", L"in-print",
+		arger::Group{ L"print", GroupId::inPrint,
 			arger::Description{ L"Print the bound printings." },
 		},
-		arger::Group{ L"reg", L"in-reg",
+		arger::Group{ L"reg", GroupId::inReg,
 			arger::Abbreviation{ L'r' },
 			arger::Description{ L"Print the current register bank." },
 		},
-		arger::Group{ L"insts", L"in-inst",
+		arger::Group{ L"insts", GroupId::inInst,
 			arger::Abbreviation{ L'i' },
 			arger::Description{ L"Print instructions ad the address." },
 			arger::Require::AtLeast(1),
 			arger::Positional{ L"count", arger::Primitive::unum, L"Number of instructions to print.", arger::Value{ 1 } },
 			arger::Positional{ L"address", arger::Primitive::any, L"Instruction to start printing from (defaults to pc)." },
 		},
-		arger::Group{ L"byte", L"in-mem8",
+		arger::Group{ L"byte", GroupId::inMem8,
 			arger::Abbreviation{ L'b' },
 			arger::Description{ L"Print bytes at the address." },
 			arger::Positional{ L"address", arger::Primitive::any, L"Address to print memory from." },
 			arger::Positional{ L"count", arger::Primitive::unum, L"Number of bytes to print." },
 		},
-		arger::Group{ L"word", L"in-mem16",
+		arger::Group{ L"word", GroupId::inMem16,
 			arger::Abbreviation{ L'w' },
 			arger::Description{ L"Print words at the address." },
 			arger::Positional{ L"address", arger::Primitive::any, L"Address to print memory from." },
 			arger::Positional{ L"count", arger::Primitive::unum, L"Number of words to print." },
 		},
-		arger::Group{ L"dword", L"in-mem32",
+		arger::Group{ L"dword", GroupId::inMem32,
 			arger::Abbreviation{ L'd' },
 			arger::Description{ L"Print dwords at the address." },
 			arger::Positional{ L"address", arger::Primitive::any, L"Address to print memory from." },
 			arger::Positional{ L"count", arger::Primitive::unum, L"Number of dwords to print." },
 		},
-		arger::Group{ L"qword", L"in-mem64",
+		arger::Group{ L"qword", GroupId::inMem64,
 			arger::Abbreviation{ L'q' },
 			arger::Description{ L"Print qwords at the address." },
 			arger::Positional{ L"address", arger::Primitive::any, L"Address to print memory from." },
 			arger::Positional{ L"count", arger::Primitive::unum, L"Number of qwords to print." },
 		},
-		arger::Group{ L"eval", L"in-eval",
+		arger::Group{ L"eval", GroupId::inEval,
 			arger::Abbreviation{ L'e' },
 			arger::Description{ L"Evaluate the value and print it." },
 			arger::Positional{ L"value", arger::Primitive::any, L"Value to be evaluated." },
 			arger::Positional{ L"hex", arger::Primitive::boolean, L"Print number as hex.", arger::Value{ true } },
-			arger::Option{ L"desc",
+			arger::Option{ L"desc", OptionId::description,
 				arger::Payload{ L"text", arger::Primitive::any },
 				arger::Description{ L"Add a pre-printed description to the evaluation." },
 			},
 		},
 	},
-	arger::Group{ L"echo", L"",
+	arger::Group{ L"echo", GroupId::echo,
 		arger::Abbreviation{ L'e' },
 		arger::Description{ L"Echo the given message." },
 		arger::Positional{ L"msg", arger::Primitive::any, L"Message to be echoed." },
 	},
-	arger::Group{ L"common", L"",
+	arger::Group{ L"common", GroupId::common,
 		arger::Description{ L"Bind the common debug printing." },
 		arger::Require::Any(),
 		arger::Positional{ L"sp-name", arger::Primitive::any, L"Name of stack pointer register." },
 	},
-	arger::Group{ L"del", L"",
+	arger::Group{ L"del",
 		arger::Description{ L"Remove breakpoints or print bindings." },
-		arger::Group{ L"break", L"break-remove",
+		arger::Group{ L"break", GroupId::delBreak,
 			arger::Abbreviation{ L'b' },
 			arger::Description{ L"Remove breakpoint with the given index." },
 			arger::Positional{ L"index", arger::Primitive::unum, L"Index of the breakpoint." },
 		},
-		arger::Group{ L"print", L"print-remove",
+		arger::Group{ L"print", GroupId::delPrint,
 			arger::Abbreviation{ L'p' },
 			arger::Description{ L"Remove bound print with the given index." },
 			arger::Positional{ L"index", arger::Primitive::unum, L"Index of the binding." },
 		},
 	},
-	arger::Group{ L"break", L"break-add",
+	arger::Group{ L"break", GroupId::addBreak,
 		arger::Abbreviation{ L'b' },
 		arger::Description{ L"Add a breakpoint to the given address." },
 		arger::Positional{ L"address", arger::Primitive::any, L"Address of the breakpoint." },
@@ -184,91 +190,68 @@ static arger::Config Commands{
 static sys::Debugger* debugger = 0;
 
 static void HandleDebug(const arger::Parsed& out) {
-	/* check if the program should take a number of steps */
-	if (out.groupId() == L"step") {
+	bool bind = out.flag(OptionId::bind);
+
+	/* handle all of the debug-related arguments */
+	switch (out.idAsEnum<GroupId>()) {
+	case GroupId::step:
 		debugger->step(out.positional(0).value().unum());
 		return;
-	}
-
-	/* check if the program should just run */
-	if (out.groupId() == L"run") {
+	case GroupId::run:
 		debugger->run();
 		return;
-	}
-
-	/* check if the program should run until an address has been reached */
-	if (out.groupId() == L"until") {
+	case GroupId::until:
 		debugger->until(str::u8::To(out.positional(0).value().str()));
 		return;
-	}
-
-	/* check if breakpoints should be added */
-	if (out.groupId() == L"break-add") {
+	case GroupId::addBreak:
 		debugger->addBreak(str::u8::To(out.positional(0).value().str()));
 		return;
-	}
-
-	/* check if breakpoints or bindings should be removed */
-	if (out.groupId() == L"break-remove") {
+	case GroupId::delBreak:
 		debugger->dropBreak(out.positional(0).value().unum());
 		return;
-	}
-	if (out.groupId() == L"bind-remove") {
+	case GroupId::delPrint:
 		debugger->dropBinding(out.positional(0).value().unum());
 		return;
-	}
-
-	/* check if the common bindings should be set up */
-	if (out.groupId() == L"common") {
+	case GroupId::common:
 		if (out.positionals() == 0)
 			debugger->setupCommon(std::nullopt);
 		else
 			debugger->setupCommon(str::u8::To(out.positional(0).value().str()));
 		return;
-	}
-
-	/* check if the debug-state should be inspected */
-	if (out.groupId() == L"in-break") {
+	case GroupId::inBreak:
 		debugger->printBreaks();
 		return;
-	}
-	if (out.groupId() == L"in-print") {
+	case GroupId::inPrint:
 		debugger->printBindings();
 		return;
-	}
-
-	/* check if the state should be inspected and potentially bound */
-	bool bind = out.flag(L"bind");
-	if (out.groupId() == L"in-reg") {
+	case GroupId::inReg:
 		debugger->printState(bind);
 		return;
-	}
-	if (out.groupId() == L"in-inst") {
+	case GroupId::inInst:
 		if (out.positionals() == 2)
 			debugger->printInstructions(str::u8::To(out.positional(1).value().str()), out.positional(0).value().unum(), bind);
 		else
 			debugger->printInstructions(std::nullopt, out.positional(0).value().unum(), bind);
 		return;
-	}
-	if (out.groupId() == L"in-mem8") {
+	case GroupId::inMem8:
 		debugger->printData8(str::u8::To(out.positional(0).value().str()), out.positional(1).value().unum() * 1, bind);
 		return;
-	}
-	if (out.groupId() == L"in-mem16") {
+	case GroupId::inMem16:
 		debugger->printData16(str::u8::To(out.positional(0).value().str()), out.positional(1).value().unum() * 2, bind);
 		return;
-	}
-	if (out.groupId() == L"in-mem32") {
+	case GroupId::inMem32:
 		debugger->printData32(str::u8::To(out.positional(0).value().str()), out.positional(1).value().unum() * 4, bind);
 		return;
-	}
-	if (out.groupId() == L"in-mem64") {
+	case GroupId::inMem64:
 		debugger->printData64(str::u8::To(out.positional(0).value().str()), out.positional(1).value().unum() * 8, bind);
 		return;
-	}
-	else if (out.groupId() == L"in-eval") {
-		auto msg = out.option(L"desc");
+	case GroupId::inEval: {
+		auto msg = out.option(OptionId::description);
 		debugger->printEval((msg.has_value() ? str::u8::To(msg.value().str()) : u8""), str::u8::To(out.positional(0).value().str()), out.positional(1).value().boolean(), bind);
+		return;
+	}
+	default:
+		return;
 	}
 }
 
@@ -294,20 +277,19 @@ void HandleCommand(std::u8string_view cmd) {
 	}
 
 	/* handle the create command */
-	if (out.groupId() == L"start") {
+	if (out.idAsEnum<GroupId>() == GroupId::start) {
 		if (env::Instance() != 0) {
 			util::nullLogger.error(u8"Process is already loaded.");
 			return;
 		}
 
 		/* fetch the initial configuration */
-		bool debug = out.flag(L"debug");
-		arger::Value traceValue = out.option(L"trace").value();
+		bool debug = out.flag(OptionId::debug);
 		sys::RunConfig config{
 			.binary = str::u8::To(out.positional(0).value().str()),
-			.translationDepth = uint32_t(out.option(L"depth").value().unum()),
-			.trace = gen::TraceType::none,
-			.logBlocks = out.flag(L"log")
+			.translationDepth = uint32_t(out.option(OptionId::depth).value().unum()),
+			.trace = out.option(OptionId::trace).value().asEnum<gen::TraceType>(),
+			.logBlocks = out.flag(OptionId::log)
 		};
 
 		/* collect the argument vector */
@@ -315,17 +297,9 @@ void HandleCommand(std::u8string_view cmd) {
 			config.args.push_back(str::u8::To(out.positional(i).value().str()));
 
 		/* collect the environment vector */
-		size_t envCount = out.options(L"environment");
+		size_t envCount = out.options(OptionId::environment);
 		for (size_t i = 0; i < envCount; ++i)
-			config.envs.push_back(str::u8::To(out.option(L"environment", i).value().str()));
-
-		/* extract the trace type to be used */
-		if (traceValue.str() == L"inst")
-			config.trace = gen::TraceType::instruction;
-		else if (traceValue.str() == L"chunk")
-			config.trace = gen::TraceType::chunk;
-		else if (traceValue.str() == L"block")
-			config.trace = gen::TraceType::block;
+			config.envs.push_back(str::u8::To(out.option(OptionId::environment, i).value().str()));
 
 		/* try to setup the userspace system */
 		std::unique_ptr<sys::Cpu> cpu = rv64::Cpu::New();
@@ -337,7 +311,7 @@ void HandleCommand(std::u8string_view cmd) {
 	}
 
 	/* handle the destroy command */
-	if (out.groupId() == L"destroy") {
+	if (out.idAsEnum<GroupId>() == GroupId::destroy) {
 		debugger = 0;
 		if (env::Instance() == 0)
 			util::nullLogger.error(u8"No process is currently running.");
